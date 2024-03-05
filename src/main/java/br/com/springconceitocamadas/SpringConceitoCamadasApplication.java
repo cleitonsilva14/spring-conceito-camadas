46108a1ed3a9e51c607af34568d16217dfcb0603
cleitonsilva14 <cleiton18ads@gmail.com>
2024-03-05 18:19:13 -0300

    Subindo o projeto pela primeira vez

---
+++ src/main/java/br/com/springconceitocamadas/SpringConceitoCamadasApplication.java
@@ -0,0 +1,13 @@
+package br.com.springconceitocamadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceitoCamadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceitoCamadasApplication.class, args);
+	}
+
+}
