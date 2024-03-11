53beed6fc725b1a6e4cef0443c84f5a1a2de62ef
cleitonsilva14 <cleiton18ads@gmail.com>
2024-03-11 14:38:39 -0300

    Ajuste classe UserService.java

--- src/main/java/br/com/springconceitocamadas/services/UserService.java
+++ src/main/java/br/com/springconceitocamadas/services/UserService.java
@@ -2,6 +2,7 @@ package br.com.springconceitocamadas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.springconceitocamadas.dto.UserDTO;
 import br.com.springconceitocamadas.entities.User;
@@ -13,10 +14,14 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	// Esse metodo chama findById do UserRepository
+	// e retorno o DTO para o Controller
+	
+	@Transactional(readOnly = true) // apenas para operações de leitura
 	public UserDTO findById(Long id) {
-		User entity = userRepository.findById(id).get();
-		UserDTO dto = new UserDTO(entity);
-		return dto;
+		User entity = userRepository.findById(id).get(); // O .get() acessa o objeto dentro do Optional
+		UserDTO dto = new UserDTO(entity);  //transforma o entity em DTO
+		return dto; // retorna o dto para o Controller
 	}
 	
 }
